3a5cd15d1e463e31e072411d128807f88be9f812
cosborn <caleb.osborn@vizientinc.com>
2023-09-27 15:14:51 -0500

    add in home component and set up route to the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h2>The Golden Eagle is nesting</h2>
+  <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/home/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  template:
+    `
+      <h2>The Golden Eagle is nesting</h2>
+    `,
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent {
+
+}
