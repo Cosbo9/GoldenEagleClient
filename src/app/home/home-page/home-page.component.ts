2b92c1ecd4f3c756400288d20b0f3e53167f9c96
cosborn <caleb.osborn@vizientinc.com>
2023-10-03 09:37:16 -0500

    add in some styles and templating

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -2,10 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  template:
-    `
-      <h2>The Golden Eagle is nesting</h2>
-    `,
+  template: `
+      <div class="home-container">
+          <div class="header-section">
+              <img src="../../../assets/goldenEagleLogo.png" alt="golden eagle" class="logo">
+              <p class="title">GOLDEN EAGLE TATTOO</p>
+          </div>
+          <div class="about-section">
+              <p>ABOUT US</p>
+              <p>
+                  Our artists here at Golden Eagle Tattoo are here for all your tattoo and piercing needs,
+                  specializing in
+                  american traditional style we have a great selection of art to choose from, or bring in a design of
+                  your
+                  own.
+              </p>
+          </div>
+          <div class="footer-section">
+              <!--     put page links here     -->
+          </div>
+      </div>
+  `,
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
